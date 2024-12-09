5ded0e60051457610983411301777aad7a868cdb
shriyash patil <shriyash.patil@xpressbess.com>
2024-12-09 23:26:39 +0530

    Respond to PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,8 +17,14 @@ public class Main {
           // Since the tester restarts your program quite often, setting SO_REUSEADDR
           // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
+
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+
+          // Ping response to the client.
+          OutputStream outputStream = clientSocket.getOutputStream();
+          outputStream.write("+PONG\r\n".getBytes());
+
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
