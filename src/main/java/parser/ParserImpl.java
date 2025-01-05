1369385a92be2db596de155161a14179e861c5f5
shriyash patil <shriyash.patil@xpressbess.com>
2025-01-05 12:48:55 +0530

    parse resp input

--- src/main/java/parser/ParserImpl.java
+++ src/main/java/parser/ParserImpl.java
@@ -9,41 +9,64 @@ import java.util.List;
 public class ParserImpl implements Parser{
     @Override
     public List<String> parse(BufferedReader br)  {
-            try {
-                System.out.println("Parsing the input");
-                List<String> tokens = new ArrayList<>();
-                String input = br.readLine();
-                System.out.println(input);
-                while (input!=null){
-                    System.out.println(input);
-                    if(input.charAt(0)=='*'){
-                        int arrayLength = Character.getNumericValue(input.charAt(1));
-                        for(int itr=0; itr<arrayLength; itr++){
-                            br.readLine();
-                            input = br.readLine();
-                            System.out.println(input);
-                            tokens.add(input);
-                        }
-                    }
-                    else if(input.charAt(0)=='$'){
-                        br.readLine();
-                        input = br.readLine();
-                        tokens.add(input);
-                    }
-                    else if(input.charAt(0)=='+'){
-                        tokens.add(input.substring(1));
-                    }
-                    else if(input.charAt(0)=='-'){
-                        tokens.add(input.substring(1));
-                    }
-                    input = br.readLine();
-                    System.out.println("input---------"+input);
-                }
-                return tokens;
-            }catch(IOException e){
-                System.out.println(e.getMessage());
-            }
+        List<String> tokens = new ArrayList<>();
+        String input;
 
-        return new ArrayList<>();
+        try {
+            System.out.println("Parsing the input");
+
+            while ((input = br.readLine()) != null) {
+                System.out.println("Input: " + input);
+                char type = input.charAt(0);
+                switch (type) {
+                    case '*':
+                        tokens.addAll(parseArray(br, input));
+                        break;
+                    case '$':
+                        tokens.add(parseSingleLine(br));
+                        break;
+                    case '+':
+                    case '-':
+                        tokens.add(input.substring(1));
+                        break;
+                    default:
+                        System.out.println("Unknown type: " + type);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error while parsing: " + e.getMessage());
+        }
+
+        return tokens;
+
     }
+
+    private List<String> parseArray(BufferedReader br, String input) throws IOException {
+        List<String> arrayTokens = new ArrayList<>();
+        int arrayLength = Character.getNumericValue(input.charAt(1));
+
+        for (int i = 0; i < arrayLength; i++) {
+            // Skip current line and read the next line
+            br.readLine();
+            String line = br.readLine();
+            if (line != null) {
+                System.out.println("Array element: " + line);
+                arrayTokens.add(line);
+            }
+        }
+
+        return arrayTokens;
+    }
+
+    private String parseSingleLine(BufferedReader br) throws IOException {
+        br.readLine(); // Skip current line
+        String nextLine = br.readLine();
+        if (nextLine != null) {
+            System.out.println("Single line token: " + nextLine);
+        }
+        return nextLine;
+    }
+
+
+
 }
